5f5df02dd823852002ed7fb05f27a8f9de0b316f
Vishal Amin <VishalRAmin@gmail.com>
2015-05-06 18:37:55 -0700

    Answers for Boolen

--- 02_Variables_Conditionals/starter_code/code_demo_boolean.rb
+++ 02_Variables_Conditionals/starter_code/code_demo_boolean.rb
@@ -15,23 +15,79 @@ def compare_two_numbers
 end
 
 ### #1
-puts "Is 8 x 77 greater than 600?"
+def compare_to_six_hundred
+  puts "Is 8 x 77 greater than 600? \n"
+  if (8*77) > 600
+    puts "True"
+  else
+    puts "False"
+  end
+end
 
 ### #2
-puts "Is 1 equal to (7 - 6)?"
+def compare_after_subtracting
+  puts "Is 1 equal to (7 - 6)? \n"
+  if (1) == (7-6)
+    puts "True"
+  else
+    puts "False"
+  end
+end
 
 ## 3
-puts "Is 77 greater than 50 AND (88 / 3) less than 30?"
+def compare_number_after_division
+  puts "Is 77 greater than 50 AND (88 / 3) less than 30? \n"
+  if (77) > (50) && if (88/3) < (30)
+    puts "True"
+  else
+    puts "False"
+  end
+end
 
 ## 4
-puts "Is the length of the word 'wheelbarrow' more than 10 characters long?"
+def determine_length_of_string
+  puts "Is the length of the word 'wheelbarrow' more than 10 characters long? \n"
+  if wheelbarrow.length > 10
+    puts "True"
+  else
+    puts "False"
+  end
+end
 
 ## 5
-puts "Are the amount of seconds in an hour greater than or equal to 3000?"
+def seconds_to_hours
+  puts "Are the amount of seconds in an hour greater than or equal to 3000? \n"
+  if (60*60) >= 3000
+    puts "True"
+  else
+    puts "False"
+  end
+end
 
 ## 6
-puts "Does the word 'slaughter' include the word laughter?"
+def include_another_word
+  puts "Does the word 'slaughter' include the word laughter? \n"
+  if slaughter.include?("laughter")
+    puts "True"
+  else
+    puts "False"
+  end
+end
+
+puts "*********************Methods Being Called********************************"
 
 
-#call each method
+
 compare_two_numbers
+puts "***next method***"
+compare_to_six_hundred
+puts "***next method***"
+compare_after_subtracting
+puts "***next method***"
+compare_number_after_division
+puts "***next method***"
+determine_length_of_string
+puts "***next method***"
+seconds_to_hours
+puts "***next method***"
+include_another_word
